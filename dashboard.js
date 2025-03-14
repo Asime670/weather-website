115c801797196d05a134ba866084dbdaf18bff3d
asime domitila <asimedomitila@gmail.com>
2025-03-14 03:49:11 +0100

    i have made some changes to github

---
+++ dashboard.js
@@ -0,0 +1,13 @@
+
+const currentTempElement = document.querySelector('.current-temp');
+const currentTemperature = 28; 
+currentTempElement.textContent = ${currentTemperature}°C
+const hourlyScroll = document.querySelector('.hourly-scroll');
+
+const hourlyData = [
+    { time: "Now",  temp: 28 },
+    { time: "10:00", temp: 27 },
+
+];
+
+
